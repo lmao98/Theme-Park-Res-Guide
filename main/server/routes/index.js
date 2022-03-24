5c61933371e6d5d215f46d793d818a423847f4b7
Nell Flores <nell.djmf@gmail.com>
2022-03-24 11:20:35 -0400

    connected to Express, routes: root, locations, and restaurant working

---
+++ main/server/routes/index.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const controllers = require('../controllers')
+const router = Router();
+
+router.get('/', (req, res) => res.send('This is root!'))
+
+router.get('/locations', controllers.getAllLocations)
+
+router.get('/restaurants', controllers.getAllRestaurants)
+
+
+module.exports = router;
