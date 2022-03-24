53e580d3f320768d1ae2481266c2445f5222e067
Nell Flores <nell.djmf@gmail.com>
2022-03-24 17:33:56 -0400

    minor styling changes to restaurant name and reviews

--- main/res-guide/src/components/Restaurant.jsx
+++ main/res-guide/src/components/Restaurant.jsx
@@ -5,7 +5,7 @@ const Restaurant = (props) => {
   return (
     <div className="res-name" placeholder="Restaurant Name">
       <div>
-        {props.name}
+        <h2>{props.name}</h2>
       </div>
     </div>
   )
