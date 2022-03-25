902ff4ce629523af76190f831ba544fd423d3704
Nell Flores <nell.djmf@gmail.com>
2022-03-25 10:05:18 -0400

    attempted to get post working

--- main/res-guide/src/components/RestaurantList.jsx
+++ main/res-guide/src/components/RestaurantList.jsx
@@ -8,22 +8,29 @@ const RestaurantList = (props) => {
     let ratingTarget = ''
     let newRating = ''
     let targetQuery = ''
-
+    
+    
+    
+    
     const enterRating = (e) => {
         console.log('clicked')
         let rate = prompt('enter your rating!')
         newRating = rate
         ratingTarget = (e.target.innerHTML)
-        // console.log(e.target.innerHTML)
-        // console.log(ratingTarget)
         setRestRating()
+        // let lastIndex = ratingTarget.lastIndexOf(" ");
+        // targetQuery = ratingTarget.substring(0, lastIndex);
+        // console.log(targetQuery)
+        // // console.log(e.target.innerHTML)
       }
 
+    //   //splice at space
       const setRestRating = async () => {
         targetQuery = ratingTarget.replace(/\s+/g, '');
         const res = await axios.post(`http://localhost:3001/rating/${targetQuery}/${newRating}`)
         console.log(res.data)
       }
+
 
     return (
         <div className="list-container">
@@ -35,9 +42,8 @@ const RestaurantList = (props) => {
                     name={ restItem.name }
                     vibe={ restItem.vibe }
                     ratings={ restItem.ratings }
-                    rateRest={enterRating}
+                    rateRest={enterRating}                  
                     />
-
                 ))
                 }
             </ul>
